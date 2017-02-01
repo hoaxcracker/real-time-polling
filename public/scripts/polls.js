1fa2bff9ede1b94383627052bae1dd23c35c6bca
Ian Lancaster <ianclancaster@gmail.com>
2017-02-01 12:43:21 -0700

    Implements github authentication for polls page

--- public/scripts/polls.js
+++ public/scripts/polls.js
@@ -1,7 +1,63 @@
 $(document).ready(() => {
+  authenticateUser()
   const pollId = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
   fetchPoll(pollId)
 })
+
+const authenticateUser = () => {
+  const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+    auth: {
+      params: { scope: 'openid email' } // Details: https://auth0.com/docs/scopes
+    }
+  })
+
+  lock.show()
+
+  $('.btn-logout').click((e) => {
+    e.preventDefault()
+    logout()
+  })
+
+  lock.on('authenticated', (authResult) => {
+    lock.getProfile(authResult.idToken, (error, profile) => {
+      if (error) {
+        // Handle error
+        return
+      }
+      localStorage.setItem('idToken', authResult.idToken)
+      // Display user information
+      showProfileInfo(profile)
+    })
+  })
+
+  // retrieve the profile:
+  const retrieveProfile = () => {
+    const idToken = localStorage.getItem('idToken')
+    if (idToken) {
+      lock.getProfile(idToken, (err, profile) => {
+        if (err) {
+          return alert('There was an error getting the profile: ' + err.message)
+        }
+      // Display user information
+        showProfileInfo(profile)
+      })
+    }
+  }
+
+  const showProfileInfo = (profile) => {
+    $('.nickname').text(profile.nickname)
+    $('.btn-login').hide()
+    $('.avatar').attr('src', profile.picture).show()
+    $('.btn-logout').show()
+  }
+
+  const logout = () => {
+    localStorage.removeItem('idToken')
+    window.location.href = '/'
+  }
+
+  retrieveProfile()
+}
 
 const fetchPoll = (pollId) => {
   fetch(`/api/polls/${pollId}`)
