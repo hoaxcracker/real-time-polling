ef0b43432feafbb385ad4c62d170ca6d96bd7984
Ian Lancaster <ianclancaster@gmail.com>
2017-02-01 00:31:16 -0700

    Send poll object to backend through post body

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const md5 = require('md5')
-const path = require('path')
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('../db/knexfile')[environment]
-const database = require('knex')(configuration)
+// const md5 = require('md5')
+// const path = require('path')
+// const environment = process.env.NODE_ENV || 'development'
+// const configuration = require('../db/knexfile')[environment]
+// const database = require('knex')(configuration)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -17,6 +17,13 @@ app.use(function (req, res, next) {
 
 app.set('port', process.env.PORT || 3000)
 
+app.post('/polls', (req, res) => {
+  console.log(req.body)
+})
+
+app.get('/polls', (req, res) => {
+  console.log(req.body)
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Express server is running on ${app.get('port')}.`)
