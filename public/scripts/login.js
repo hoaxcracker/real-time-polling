a6038ff988ff9af00fc7fabf8a5c383ca75db675
Ian Lancaster <ianclancaster@gmail.com>
2017-02-01 17:47:14 -0700

    Clean and refactor login.js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,62 +1,27 @@
-$(document).ready(function() {
-
+$(document).ready(() => {
   var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
     auth: {
-      params: { scope: 'openid email' } //Details: https://auth0.com/docs/scopes
+      params: { scope: 'openid email' } // Details: https://auth0.com/docs/scopes
     }
-  });
-
-  $('.btn-login').click(function(e) {
-    e.preventDefault();
-    lock.show();
-  });
-
-  $('.btn-logout').click(function(e) {
-    e.preventDefault();
-    logout();
   })
 
-  lock.on("authenticated", function(authResult) {
-    lock.getProfile(authResult.idToken, function(error, profile) {
+  lock.show()
+
+  $('.btn-logout').click((e) => {
+    e.preventDefault()
+    logout()
+  })
+
+  lock.on('authenticated', (authResult) => {
+    lock.getProfile(authResult.idToken, (error, profile) => {
       if (error) {
         // Handle error
-        return;
+        return
       }
-      localStorage.setItem('id_token', authResult.idToken);
-      localStorage.setItem('profile', JSON.stringify(profile));
-      // Display user information
-      window.location = localStorage.getItem('currentPoll');
-      show_profile_info(profile);
-    });
-  });
+      localStorage.setItem('id_token', authResult.idToken)
+      localStorage.setItem('profile', JSON.stringify(profile))
 
-  //retrieve the profile:
-  var retrieve_profile = function() {
-    var id_token = localStorage.getItem('id_token');
-    if (id_token) {
-      lock.getProfile(id_token, function (err, profile) {
-        if (err) {
-          return alert('There was an error getting the profile: ' + err.message);
-        }
-        // Display user information
-        console.log(localStorage.getItem('currentPoll'))
-        window.location = localStorage.getItem('currentPoll')
-        show_profile_info(profile);
-      });
-    }
-  };
-
-  var show_profile_info = function(profile) {
-     $('.nickname').text(profile.nickname);
-     $('.btn-login').hide();
-     $('.avatar').attr('src', profile.picture).show();
-     $('.btn-logout').show();
-  };
-
-  var logout = function() {
-    localStorage.removeItem('id_token');
-    window.location.href = "/";
-  };
-
-  retrieve_profile();
-});
+      window.location = localStorage.getItem('currentPoll')
+    })
+  })
+})
